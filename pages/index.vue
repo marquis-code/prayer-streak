e54672f3f5337fb8821a6ce750f761f959161cb7
Marquis <abahmarquis@gmail.com>
2025-01-06 00:03:38 -0800

    remove uniused

--- pages/index.vue
+++ pages/index.vue
@@ -54,10 +54,6 @@
                 build lasting habits, and deepen your connection with God. Join
                 thousands of others who have strengthened their prayer life through
                 consistent practice.</p>
-              <!-- <div class="mt-10 flex items-center gap-x-6">
-                <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
-                <a href="#" class="text-sm/6 font-semibold text-gray-900">Live demo <span aria-hidden="true">→</span></a>
-              </div> -->
               <div data-aos="fade-up" class="mt-10 flex items-center gap-x-6">
                 <NuxtLink
                   to="/get-started"
